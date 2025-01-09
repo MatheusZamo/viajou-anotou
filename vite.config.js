af56018c5fda9d9c3b1b76565d6d8369d82fad45
MatheusZamo <19matheushenrique03@gmail.com>
2025-01-09 18:32:21 -0300

    Add Images Optimizer

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { resolve } from "node:path"
 import { defineConfig } from "vite"
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer"
 import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), ViteImageOptimizer({})],
   resolve: {
     alias: [{ find: "@", replacement: resolve(__dirname, "src") }],
   },
