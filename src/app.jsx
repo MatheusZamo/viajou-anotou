5f33ae320205cbc6acb18d4fae63f774fe66e76b
MatheusZamo <19matheushenrique03@gmail.com>
2025-01-08 21:24:00 -0300

    Created the Header component and added the useLocation hook

--- src/app.jsx
+++ src/app.jsx
@@ -5,124 +5,116 @@ import {
   createBrowserRouter,
   createRoutesFromElements,
   RouterProvider,
+  useLocation,
 } from "react-router-dom"
 
-const Home = () => (
-  <main className="main-home">
-    <section>
-      <nav className="nav">
+const links = [
+  { path: "/", text: "Início" },
+  { path: "/price", text: "Preço" },
+  { path: "/about", text: "Sobre" },
+]
+
+const Header = () => {
+  const location = useLocation()
+  const isNotHomepage = location.pathname !== "/"
+
+  return (
+    <nav className="nav">
+      <Link to="/">
         <img
           className="logo"
-          src="logo-viajou-anotou-light.png"
+          src={`logo-viajou-anotou-${isNotHomepage ? "dark" : "light"}.png`}
           alt="Logo da viajou anotou"
         />
-
-        <ul>
-          <li>
-            <NavLink to="/">Início</NavLink>
-          </li>
-          <li>
-            <NavLink to="price">Preço</NavLink>
-          </li>
-          <li>
-            <NavLink to="about">Sobre</NavLink>
-          </li>
-        </ul>
-      </nav>
-      <h1>
-        Você viaja o mundo.
-        <br /> E o ViajouAnotou mantém suas aventuras anotadas.
-      </h1>
-      <h2>
-        Um mapa mundial que rastreia por onde você passou. Nunca esqueça suas
-        experiências e mostre aos seus amigos o quê você fez pelo mundo.
-      </h2>
-      <Link to="about" className="cta">
-        Começar Agora
       </Link>
-    </section>
-  </main>
+
+      <ul>
+        {links.map((link) => {
+          const linkShouldBeGray =
+            isNotHomepage && location.pathname !== link.path
+          return (
+            <li key={link.text}>
+              <NavLink
+                to={link.path}
+                style={linkShouldBeGray ? { color: "#C2C2C2" } : null}
+              >
+                {link.text}
+              </NavLink>
+            </li>
+          )
+        })}
+      </ul>
+    </nav>
+  )
+}
+
+const Home = () => (
+  <>
+    <Header />
+    <main className="main-home">
+      <section>
+        <h1>
+          Você viaja o mundo.
+          <br /> E o ViajouAnotou mantém suas aventuras anotadas.
+        </h1>
+        <h2>
+          Um mapa mundial que rastreia por onde você passou. Nunca esqueça suas
+          experiências e mostre aos seus amigos o quê você fez pelo mundo.
+        </h2>
+        <Link to="about" className="cta">
+          Começar Agora
+        </Link>
+      </section>
+    </main>
+  </>
 )
 
 const Price = () => (
-  <main className="main-pricing">
-    <nav className="nav">
-      <img
-        className="logo"
-        src="logo-viajou-anotou-dark.png"
-        alt="Logo da viajou anotou"
-      />
-
-      <ul>
-        <li>
-          <NavLink to="/">Início</NavLink>
-        </li>
-        <li>
-          <NavLink to="">Preço</NavLink>
-        </li>
-        <li>
-          <NavLink to="/about">Sobre</NavLink>
-        </li>
-      </ul>
-    </nav>
-    <section>
-      <div>
-        <h1>
-          Preço simples.
-          <br /> Só R$ 47/mês.
-        </h1>
-        <p>
-          Comece hoje mesmo a anotar suas aventuras e mostre aos seus amigos o
-          quê você fez pelo mundo.
-        </p>
-      </div>
-      <img className="img" src="preco-viajou-anotou.jpg" alt="" />
-    </section>
-  </main>
+  <>
+    <Header />
+    <main className="main-pricing">
+      <section>
+        <div>
+          <h1>
+            Preço simples.
+            <br /> Só R$ 47/mês.
+          </h1>
+          <p>
+            Comece hoje mesmo a anotar suas aventuras e mostre aos seus amigos o
+            quê você fez pelo mundo.
+          </p>
+        </div>
+        <img src="preco-viajou-anotou.jpg" alt="Avenida Paulista" />
+      </section>
+    </main>
+  </>
 )
 
 const About = () => (
-  <main className="main-about">
-    <nav className="nav">
-      <img
-        className="logo"
-        src="logo-viajou-anotou-dark.png"
-        alt="Logo da viajou anotou"
-      />
-
-      <ul>
-        <li>
-          <NavLink to="/">Início</NavLink>
-        </li>
-        <li>
-          <NavLink to="/price">Preço</NavLink>
-        </li>
-        <li>
-          <NavLink to="">Sobre</NavLink>
-        </li>
-      </ul>
-    </nav>
-
-    <section>
-      <div>
-        <h1>Sobre o ViajouAnotou.</h1>
-        <p>
-          O ViajouAnotou nasceu do desejo dos amigos Paulo e Roberto de
-          compartilharem de forma rápida suas aventuras pelo mundo.
-        </p>
-        <p>
-          Aos poucos, esse desejo virou realidade em forma de software entre
-          amigos e familiares. Hoje, você também pode ser parte dessa
-          comunidade.
-        </p>
-      </div>
-      <img
-        className="img"
-        src="sobre-viajou-anotou.jpg"
-        alt="Logo da viajou anotou"
-      />
-    </section>
-  </main>
+  <>
+    <Header />
+    <main className="main-about">
+      <section>
+        <div>
+          <h1>Sobre o ViajouAnotou.</h1>
+          <p>
+            O ViajouAnotou nasceu do desejo dos amigos Paulo e Roberto de
+            compartilharem de forma rápida suas aventuras pelo mundo.
+          </p>
+          <p>
+            Aos poucos, esse desejo virou realidade em forma de software entre
+            amigos e familiares. Hoje, você também pode ser parte dessa
+            comunidade.
+          </p>
+        </div>
+        <img
+          className="img"
+          src="sobre-viajou-anotou.jpg"
+          alt="Logo da viajou anotou"
+        />
+      </section>
+    </main>
+  </>
 )
 
 const NotFound = () => (
